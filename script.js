6f9bc48ce60b96ae25379fd5f6f63d62134d2ea4
wuyeelok <wuyeelok@gmail.com>
2024-03-05 10:59:33 +0800

    Remove old console log

--- script.js
+++ script.js
@@ -55,7 +55,6 @@ function onClickItem(e) {
 }
 
 function removeItem(item) {
-  console.log(item);
   /* if (e.target.parentElement.classList.contains("remove-item")) {
     if (confirm("Are you sure?")) {
       e.target.parentElement.parentElement.remove();
